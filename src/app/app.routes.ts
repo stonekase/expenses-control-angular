952fcc07eb4f885f48f9b3a71f38fa4b9965713f
stonekase <cybrat@gmail.com>
2024-03-22 14:07:36 +0100

    ci first push test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,3 +5,4 @@ export const routes: Routes = [];
 
 //git config --global user.email "cybrat@gmail.com"
 //git config --global user.name "stonekase"
+// CI Token :1//030bPPMtT6ioRCgYIARAAGAMSNwF-L9IrGhQxNR82ahnwXO46hdAHU9eG2xpzmkNXLeYUsTbo8zQtThyv_qn36CGgtx4NzWzDGag
